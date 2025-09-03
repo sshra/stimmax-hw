a932b4bae6c277f561eac76d9841ed320dcc8acc
Yuriy Korol as shra <sshra@mail.ru>
2025-09-03 14:31:23 +0500

    update

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,11 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
+    val projectName = "Kotlin Lesson"
+    val userName = "Ssshra"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    println("Hello, " + userName + "! This is " + projectName)
 
     for (i in 1..10) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
